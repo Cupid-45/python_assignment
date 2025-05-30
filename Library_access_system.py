3c2e5ecdbd8f7bd6ff482e7abf5af1a54a9208dd
Cupid-45 <mokshamadhu2006@gmail.com>
2025-05-30 22:50:20 +0530

    Library_access_system.py

--- Library_access_system.py
+++ Library_access_system.py
@@ -5,25 +5,25 @@ class Member:
                  self.name=name
                  self.id=id
 
-class StudentMember:
-          def __init__(self,name,id): 
-                super().__init__(name,id)
-                self.books_borrowed=0
+class StudentMember(Member):
+       def __init__(self,name,id): 
+              super().__init__(name,id)
+              self.books_borrowed=0
 
-          def add_book(self):
+       def add_book(self):
                   self.books_borrowed+=1
 
-          def return_books(self):
+       def return_books(self):
             if self.books_borrowed>0:
                 self.books_borrowed-=1
 
-def display_status(self):
-       print(self.name,"has borrowed",self.books_borrowed,"books")
+       def display_status(self):
+              print(self.name,"has borrowed",self.books_borrowed,"books")
 
 student=StudentMember('Rahul','M101')
 student.add_book()
-student.add.book()
-student.return_book()
+student.add_book()
+student.return_books()
 student.display_status()
 
 
