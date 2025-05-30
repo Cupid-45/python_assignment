3ae88f6a014994ec96a0dd0833f0fe14656297ec
Cupid-45 <mokshamadhu2006@gmail.com>
2025-05-30 23:25:09 +0530

    Update Online_learning_platform.py

--- Online_learning_platform.py
+++ Online_learning_platform.py
@@ -1,32 +1,43 @@
-3.Online Learning platform
+#3.Online Learning platform
 
 class User:
-          def __init__(self,name,email)
+       def __init__(self,name,email):
                 self.name=name
                 self.email=email
 
-        def display_info(self)
+       def display_info(self):
                print("User: ",self.name,"Email: ",self.email)
 
 class Instructor(User):
-          def __init__(self,name,email,subjects):
-                 super().__init__(name,email)
-                 self.subject=subject
+       def __init__(self,name,email,subjects):
+              super().__init__(name,email)
+              self.subjects=subjects
 
-         def upload_content(self):
-                print(self.name,"upload content on",self.subject)
-
-        def display_info(self):
-                print("instructor:",self.name,"Subject: ",self.subject)
-              
-class CourseCreator(Instructor):
-          def __init__(self,name,email,subject,modules)
-                super().__init__(self,name,email,subject)
-                self.modules=modules
-        def create(self):
-               print("Creating course")
-               for module in self.modules:
-                       print("-",module)
+       def upload_content(self):
+              print(self.name,"upload content on",self.subjects)
 
        def display_info(self):
-               print("CourseCreator: ",self.name,"Modules: ",self.modules)
+              print("instructor:",self.name,"Subject: ",self.subjects)
+              
+class CourseCreator(Instructor):
+       def __init__(self,name,email,subjects,modules):
+              super().__init__(name,email,subjects)
+              self.modules=modules
+       def create(self):
+              print("Creating course")
+              for module in self.modules:
+                     print("-",module)
+
+       def display_info(self):
+              print("CourseCreator: ",self.name,"Modules: ",self.modules)
+       
+user = User("Anita", "anita@mail.com")
+user.display_info()
+
+instructor = Instructor("Raj", "raj@mail.com", "Math")
+instructor.display_info()
+instructor.upload_content()
+
+creator = CourseCreator("Kiran", "kiran@mail.com", "Science", ["Module1", "Module2"])
+creator.display_info()
+creator.create()
