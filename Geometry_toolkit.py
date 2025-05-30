29c9b0867124563cc2c565e975aafc7f8808e0ea
Cupid-45 <mokshamadhu2006@gmail.com>
2025-05-30 21:04:32 +0530

    Create Geometry_toolkit.py

---
+++ Geometry_toolkit.py
@@ -0,0 +1,10 @@
+class shapecalc:
+         def area(self,a,b=none):
+         if b is None:
+                 return math.pi*a*a
+         else:
+                return a*b
+
+calc=shapecalc()
+print("Area of circle: ",calc.area(5))
+print("Area of rectangle: ",calc.area(4,6))
