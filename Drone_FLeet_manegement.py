4e9e450ab30f69870e840844496a1dee97cf6166
Cupid-45 <mokshamadhu2006@gmail.com>
2025-05-30 23:04:12 +0530

    Drone_FLeet_manegement.py

--- Drone_FLeet_manegement.py
+++ Drone_FLeet_manegement.py
@@ -1,19 +1,20 @@
 class Device:
-         def __init__(self,id)
-         self.id=id
+       def __init__(self,id):
+              self.id=id
 
 class Flyer:
-          def fly(self):
-                 print("FLying.....")
+       def fly(self):
+              print("FLying.....")
 
-class Drone(Device,flyer):
-          def __init__(self,id)
+class Drone(Device,Flyer):
+       def __init__(self,id):
+              Device.__init__(self,id)
+                      
+       def capture_image(self):
+              print("Capturing image....")
 
-          def capture_image(self):
-                 print("Capturing image....")
-
-d=drone
-print("Device id: ",d,device_id)
+d=Drone("g545")
+print("Device id: ",d,id)
 d.fly()
 d.capture_image()
 
