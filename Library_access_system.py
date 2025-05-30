9b62920890b6416e51fbbd58dec112caddb3643f
Cupid-45 <mokshamadhu2006@gmail.com>
2025-05-30 21:00:32 +0530

    Create Library_access_system.py

---
+++ Library_access_system.py
@@ -0,0 +1,29 @@
+# 1. Library Access System
+
+class Member:
+          def __init__(self,name,id):
+                 self.name=name
+                 self.id=id
+
+class StudentMember:
+          def __init__(self,name,id): 
+                super().__init__(name,id)
+                self.books_borrowed=0
+
+          def add_book(self):
+                  self.books_borrowed+=1
+
+          def return_books(self):
+            if self.books_borrowed>0:
+                self.books_borrowed-=1
+
+def display_status(self):
+       print(self.name,"has borrowed",self.books_borrowed,"books")
+
+student=StudentMember('Rahul','M101')
+student.add_book()
+student.add.book()
+student.return_book()
+student.display_status()
+
+
