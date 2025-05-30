9e362c6c1453bc65c798cb4e58a23ba1b0d78675
Cupid-45 <mokshamadhu2006@gmail.com>
2025-05-30 23:29:19 +0530

    smart_home_appliance.py

--- smart_home_appliance.py
+++ smart_home_appliance.py
@@ -2,15 +2,19 @@ class Appliance:
           def status(self):
                  print("General appliance is working")
 
-class Fan(Appliance)
+class Fan(Appliance):
           def status(self):
                  print("Fan is running")
 
 class Light(Appliance):
           def status(self):
-                  print("Ac is cooling")
+                  print("Light is on")
+
+class AC(Appliance):
+        def status(self):
+                print("AC is cooling")
 
 device=[Fan(),Light(),AC()]
-for device in devices:
+for device in device:
       device.status()
 
